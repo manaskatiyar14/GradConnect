4d8a402d95ba251cb4372f4e1845126f3290c8ad
JindNagendraa <nagenderjind@gmail.com>
2026-05-09 04:48:01 +0530

    updated internship section

--- grad-connect/src/pages/Teams.jsx
+++ grad-connect/src/pages/Teams.jsx
@@ -1,25 +1,48 @@
-import { useState } from "react";
-import CreateTeam from "../component/CreateTeam";
-import TeamList from "../component/TeamList";
-import StudentList from "../component/StudentList";
+import { useEffect, useState } from "react";
 import "../App.css";
 
-export default function Teams() {
-  const [teams, setTeams] = useState([]);
+export default function Internships() {
+  const [internshipData, setInternshipData] = useState([]);
+  useEffect(() => {
+    fetch("https://gradinternshipapi.onrender.com/api/cars")
+      .then((res) => res.json())
+      .then((data) => setInternshipData(data))
+      .catch((err) => console.log(err));
+  }, []);
 
-  const addTeam = (team) => {
-    setTeams([...teams, team]);
+  const applyInternship = (company) => {
+    alert("Applied to " + company);
   };
-
-  const joinTeam = (index) => {
-    alert("Joined " + teams[index].name);
-  };
-
   return (
-    <div className="teams-page">
-      <CreateTeam addTeam={addTeam} />
-      <TeamList teams={teams} joinTeam={joinTeam} />
-      <StudentList />
+    <div className="internship-page">
+      <h1 className="internship-title">
+        Internship Opportunities
+     </h1>
+      <div className="internship-grid">
+      {internshipData.map((internship) => (
+       <div className="internship-card" key={internship.id}>
+            <h2>{internship.company}</h2>
+            <p>
+              <strong>Role:</strong> {internship.role}
+            </p>
+            <p>
+              <strong>Duration:</strong> {internship.duration}
+            </p>
+            <p>
+              <strong>Stipend:</strong> {internship.stipend}
+            </p>
+            <p>
+              <strong>Location:</strong> {internship.location}
+            </p>
+            <button
+              className="apply-btn"
+              onClick={() => applyInternship(internship.company)}
+            >
+              Apply Now
+         </button>
+        </div>
+      ))}
+      </div>
     </div>
   );
 }
